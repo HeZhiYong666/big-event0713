2bfb85e2cf6ddc11f6e4af659b26666e208786e1
HeZhiYong666 <1051839602@qq.com>
2020-07-13 19:25:29 +0800

    退出事件完成

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,7 +3,7 @@ $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url;
     // 统一配置 conplete
     options.complete = function(xhr) {
-        if (xhr.responseJSON.status === 1) {
+        if (xhr.responseJSON.status === 1 && xhr.responseJSON.message === '身份认证失败！') {
             // 删除假token
             localStorage.removeItem('token');
             // 跳转到登录页
